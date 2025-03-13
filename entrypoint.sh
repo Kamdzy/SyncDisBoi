fa7da46d1f725224925f577079c309aef54e1352
Kamdzy <28660325+Kamdzy@users.noreply.github.com>
2025-03-13 22:49:38 +0100

    add logs for entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,9 +11,11 @@ fi
 
 # Run the script using env variables if they're set either from args.ini or from the docker run command
 if [ -n "$SRC_PLATFORM" ] && [ -n "$DST_PLATFORM" ]; then
+    echo "Executing: /usr/local/bin/sync_dis_boi $SRC_PLATFORM $DST_PLATFORM $@"
     exec /usr/local/bin/sync_dis_boi $SRC_PLATFORM $DST_PLATFORM $@
     exit 0
 fi
 
 # Pass the arguments as extra parameters from docker run if the env variables are not set
+echo "Executing: /usr/local/bin/sync_dis_boi $@"
 exec /usr/local/bin/sync_dis_boi $@
