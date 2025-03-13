485bdc4e74ff37b172dd234583e7c3f691511881
Kamdzy <28660325+Kamdzy@users.noreply.github.com>
2025-03-13 16:41:51 +0100

    Enhance entrypoint script to dynamically load env variables from args.ini and support additional parameters from docker run

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,16 @@
 #!/usr/bin/env bash
+
+# Optional
+# Dynamically load the env variables from the args.ini file
+if [ -f "$HOME/.config/SyncDisBoi/args.ini" ]; then
+    source "$HOME/.config/SyncDisBoi/args.ini"
+fi
+
+# Run the script using env variables if they're set either from args.ini or from the docker run command
+if [ -n "$SRC_PLATFORM" ] && [ -n "$DST_PLATFORM" ]; then
+    exec /usr/local/bin/sync_dis_boi "$SRC_PLATFORM" "$DST_PLATFORM" "$@"
+    exit 0
+fi
+
+# Pass the arguments as extra parameters from docker run if the env variables are not set
 exec /usr/local/bin/sync_dis_boi "$@"
