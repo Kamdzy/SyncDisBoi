f98fa17687020aed0ec30e9dbb31b1809bc88597
Kamdzy <28660325+Kamdzy@users.noreply.github.com>
2025-07-01 09:06:45 +0200

    fix: adjust sleep trigger from 300 to 200 songs in synchronization process

--- src/sync.rs
+++ src/sync.rs
@@ -144,9 +144,9 @@ pub async fn synchronize(
             if dst_api.api_type() == MusicApiType::YtMusic {
                 unsafe {
                     SONG_COUNTER += 1;
-                    if SONG_COUNTER % 300 == 0 {
+                    if SONG_COUNTER % 200 == 0 {
                         let sleep_duration = SLEEP_DURATION;
-                        info!("Reached 300 songs, taking a {}-second break...", sleep_duration);
+                        info!("Reached 200 songs, taking a {}-second break...", sleep_duration);
                         sleep(Duration::from_secs(sleep_duration)).await;
                         SLEEP_DURATION += 60; // Add 60 seconds to the sleep duration each time
                     }
